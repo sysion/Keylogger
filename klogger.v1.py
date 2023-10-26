597a603abcf5368d90ce937992fcadade75c8206
sysionng <sysionng@gmail.com>
2023-10-26 15:26:57 +0100

    completed keylogger - initial attempt

---
+++ klogger.v1.py
@@ -0,0 +1,26 @@
+import sys,os
+
+kfile=os.path.join(os.path.dirname(os.path.abspath(__file__)),'.kfile')
+
+class KeyLogger:
+
+	def whichKey(self):
+		print('keyboard event captured')
+		print(KeyLogger.__dict__)
+
+	def captureKey(self):
+		with open(kfile,'a') as f:
+			ckey=input()
+			f.write(ckey+'\n')
+		print(ckey)
+
+def klog():
+	if not os.path.exists(kfile):
+		with open(kfile, 'w+'): pass    # create a new file if not exists
+
+	kl=KeyLogger()
+	#kl.whichKey()
+	kl.captureKey()
+
+if __name__=='__main__':
+	klog()
---
+++ klogger.v2.py
@@ -0,0 +1,26 @@
+import sys,os,logging
+
+kfile=os.path.join(os.path.dirname(os.path.abspath(__file__)),'.kfile')
+
+class KeyLogger:
+
+	def whichKey(self):
+		print('keyboard event captured')
+		print(KeyLogger.__dict__)
+
+	def captureKey(self):
+		with open(kfile,'a') as f:
+			ckey=input()
+			f.write(ckey+'\n')
+		print(ckey)
+
+def klog():
+	if not os.path.exists(kfile):
+		with open(kfile, 'a+'): pass    # create a new file if not exists
+
+	kl=KeyLogger()
+	#kl.whichKey()
+	kl.captureKey()
+
+if __name__=='__main__':
+	klog()
